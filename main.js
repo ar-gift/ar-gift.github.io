7b23b9260eeb8732157dc396c21aafc880be9aed
olegskvortsov <o.skvortsov.developer@gmail.com>
2019-08-04 01:17:46 +0300

    on sound

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-AFRAME.registerComponent('trigger', {
-  init: function() {
-    setInterval(() => {
-      this.el.emit('starFireworks', {
-        position: new THREE.Vector3(
-          -2 + Math.random() * 4,
-          0
-          -2 + Math.random() * 4
-        ),
-        rotation: new THREE.Vector3(-90, 0, 0)
-      });
-    }, 1000)
-  }
-});
-
 var playing = false;
 var hbSound = new Audio("./assets/happy-birthday.wav");
 
@@ -27,15 +12,13 @@ AFRAME.registerComponent('markerhandler', {
     // Set up the tick throttling. Will check if marker is active every 500ms
     this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
   },
-  tick: function(t, dt) {
-
+  tick: function() {
+    console.log(document.querySelector("#hb-marker").object3D);
     if (document.querySelector("#hb-marker").object3D.visible && playing) {
-      // MARKER IS PRESENT
       hbSound.play();
       playing = true;
     } else {
       playing = false;
     }
-
   }
 });
