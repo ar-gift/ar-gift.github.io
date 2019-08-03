c2ddda73cfc839dbf0cb3ea24f99d011cd83e79b
olegskvortsov <o.skvortsov.developer@gmail.com>
2019-08-03 22:15:19 +0300

    add fireworks

---
+++ main.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('trigger', {
+  init: function() {
+    setInterval(() => {
+      this.el.emit('starFireworks', {
+        position: new THREE.Vector3(
+          -2 + Math.random() * 4,
+          0
+          -2 + Math.random() * 4
+        ),
+        rotation: new THREE.Vector3(-90, 0, 0)
+      });
+      console.log("asd");
+    }, 500)
+  },
+});
