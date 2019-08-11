834ac6694d65d49d884bd1a4085d5fd7e768ad20
olegskvortsov <o.skvortsov.developer@gmail.com>
2019-08-11 10:43:24 +0300

    add support safari

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var playing = false;
 var hbSound = new Audio("https://ar-gift.github.io/assets/happy-birthday.mp3");
 
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+function playSafari() {
+  isSafari && hbSound.play();
+}
+
 //Detect end of audio
 hbSound.addEventListener("ended", function() {
   hbSound.currentTime = 0;
