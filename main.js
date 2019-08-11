b11688586938a15656742e6ff0902276a1bb0109
olegskvortsov <o.skvortsov.developer@gmail.com>
2019-08-11 10:47:57 +0300

    add support safari

--- main.js
+++ main.js
@@ -7,30 +7,32 @@ function playSafari() {
   isSafari && hbSound.play();
 }
 
-//Detect end of audio
-hbSound.addEventListener("ended", function() {
-  hbSound.currentTime = 0;
-  if (playing) {
-    hbSound.play();
-  }
-});
-
-AFRAME.registerComponent('markerhandler', {
-  init: function() {
-    // Set up the tick throttling. Will check if marker is active every 500ms
-    this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
-  },
-  tick: function() {
-    if (isSafari) return;
-    if (document.querySelector("#hb-marker").object3D.visible && !playing) {
+if (!isSafari) {
+  //Detect end of audio
+  hbSound.addEventListener("ended", function() {
+    hbSound.currentTime = 0;
+    if (playing) {
       hbSound.play();
-      playing = true;
-    } else {
-      playing = document.querySelector("#hb-marker").object3D.visible;
-      if (!playing) {
-        hbSound.pause();
-        hbSound.currentTime = 0;
-      };
     }
-  }
-});
+  });
+
+  AFRAME.registerComponent('markerhandler', {
+    init: function() {
+      // Set up the tick throttling. Will check if marker is active every 500ms
+      this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
+    },
+    tick: function() {
+      if (document.querySelector("#hb-marker").object3D.visible && !playing) {
+        hbSound.play();
+        playing = true;
+      } else {
+        playing = document.querySelector("#hb-marker").object3D.visible;
+        if (!playing) {
+          hbSound.pause();
+          hbSound.currentTime = 0;
+        };
+      }
+    }
+  });
+
+}
